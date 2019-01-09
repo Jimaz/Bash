68594278dda35c638a885a0b6890d683c705ff6f
Jimaz <31802939+Jimaz@users.noreply.github.com>
2019-01-09 16:17:05 +0100

    Update checkpath.sh

--- checkpath.sh
+++ checkpath.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [[ ! :$PATH: == *"$HOME/tests/hello/"* ]]; then
-	# oops, the directory is not on the path
+	# The directory is not on the path 
 	read -rp 'Input full path for program: ' hello
 	echo
 
 	export PATH=$PATH:$hello
 	echo "GREAT! Now the path has been exported"
 else
-	# OK, the directory is on the path
-	echo "Path already exported. Nothing to do... I0I"
+	# Allright, the directory is on the path
+	echo "Path already exported. Nothing to do... o^o"
 
 fi
 
